82892a73d32b40e3b61441bea51743ae1d5f1163
NayeemHossenJim <155984091+NayeemHossenJim@users.noreply.github.com>
2024-02-04 12:58:00 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Lab_Report_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 4, b = 4, c = 4;
+            Console.WriteLine("Enter the length of side 1 : " + a);
+            Console.WriteLine("Enter the length of side 2 : " + b);
+            Console.WriteLine("Enter the length of side 3 : " + c);
+            string Triangle = (a == b && b == c) ? "Triangle Classification : Equilateral Triangle" :
+                (a == b || b == c) ? "Triangle Classification : Isoscles Triangle" : 
+                (a!=b && b!=c && a!=c) ? "Triangle Classification : Scalene Triangle" : " ";
+            Console.WriteLine();
+            Console.WriteLine(Triangle);
+            Console.Read();
+        }
+    }
+}
